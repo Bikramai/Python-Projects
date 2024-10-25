7eb8544a95f5172ec71cd070c1ba99ebfc2d0beb
bikramia <bikram.rhce@gmail.com>
2024-10-24 20:17:07 -0400

    Currency Converter - added project and Mapping the logic of the project

---
+++ Currency-Converter/currency_converter.py
@@ -0,0 +1,17 @@
+# loop 
+    # Ask the user for an amount
+    # If invalid
+    #   Print an error
+
+# loop
+    # Ask for the source currency
+    # If invalid
+    #   Print an error
+
+# loop
+    # Ask for the target currency
+    # If invalid
+    #   Print an error
+
+# Do the conversion
+# Print the result
