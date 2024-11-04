6c6eeb9ffa59c50a6328c114d04fa73e7c2db2dc
bikramia <bikram.rhce@gmail.com>
2024-11-04 18:18:58 -0500

    Mapping  Logic in the Quiz Game Project

---
+++ Quiz-Game/quiz_game.py
@@ -0,0 +1,13 @@
+# Mapping out the logic
+
+# Define questions, options, correct answer
+# Shuffle the questions
+# Loop over question
+#   Print question
+#   Get user input
+#   If input is correct
+#       Print Correct answer
+#       Increase score
+#   Else
+#       Print wrong answer
+# Print the final score
