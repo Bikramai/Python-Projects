2bf0470fb388846d32d8804c650b9e2ce986241c
bikramia <bikram.rhce@gmail.com>
2024-11-09 22:47:59 -0500

    Completed Project - quiz game

--- Quiz-Game/quiz_game.py
+++ Quiz-Game/quiz_game.py
@@ -11,3 +11,43 @@
 #   Else
 #       Print wrong answer
 # Print the final score
+
+import random
+
+quiz = [
+    {
+        'question': 'What is the capital of France?',
+        'options': ['A. Berlin', 'B. Madrid', 'C. Paris', 'D. Rome'],
+        'answer': 'C'
+    },
+    {
+        'question': 'Which planet is known as red planet?',
+        'options': ['A. Earth', 'B. Mars', 'C. Jupiter', 'D. Saturn'],
+        'answer': 'C'
+    },
+    {
+        'question': 'What is the largest ocean on Earth?',
+        'options': ['A. Atlantic', 'B. Indian', 'C. Arctic', 'D. Pacific'],
+        'answer': 'D'
+    }
+]
+
+random.shuffle(quiz)
+
+
+score = 0
+
+for index, item in enumerate(quiz, 1):
+    print(f'Question {index}: {item['question']}')
+    for option in item['options']:
+        print(option)
+
+    answer = input('Your answer: ').upper().strip()
+
+    if answer == item['answer']:
+        print('Correct!')
+        score += 1
+    else:
+        print(f'Wrong! the correct answer is {item['answer']}')
+
+print(f'Quiz over! Your final score is {score} out of {len(quiz)}')
