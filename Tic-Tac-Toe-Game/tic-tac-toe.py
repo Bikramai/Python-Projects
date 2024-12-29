1a85d36750a382ec1671753f05fa2aa67df5b490
bikramia <bikram.rhce@gmail.com>
2024-12-28 21:48:08 -0500

    Project Tic-Tac-Toe -testing

--- Tic-Tac-Toe-Game/tic-tac-toe.py
+++ Tic-Tac-Toe-Game/tic-tac-toe.py
@@ -14,3 +14,34 @@
 #       Print a message
 #       Break
 #   Switch the current Player
+
+# Solution
+board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+def print_board(board):
+    line = ('---+---+---')
+    print(line)
+    for row in board:
+        print(f' {row[0]} | {row[1]} | {row[2]} ')
+        print(line)
+
+    current_player = 'X'
+
+    while True:
+        print(f"Palyer {current_player}'s turn")
+        row = int(input('Enter row (0-2): '))
+        col = int(input('Enter col (0-2): '))
+
+        if board[row][col] == ' ': # Check if the cell is empty
+            board[row][col] = current_player
+        else:
+            print('This cell is already taken')
+            continue
+
+        print_board(board)
+
+print_board(board)
