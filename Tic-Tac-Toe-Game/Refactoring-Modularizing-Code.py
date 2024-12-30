b826ba171c4df7fe6bfd40d30d600426be1c910a
bikramia <bikram.rhce@gmail.com>
2024-12-30 15:33:55 -0500

    Refactoring-Modularizing code

--- Tic-Tac-Toe-Game/Refactoring-Modularizing-Code.py
+++ Tic-Tac-Toe-Game/Refactoring-Modularizing-Code.py
@@ -1,21 +1,3 @@
-# Mapping the logic
-# Declare the board
-# Print the board
-# Current player = 'X'
-# Loop
-#   Ask the current player for a move
-#   Store their mark on the board
-#   Print the board
-#   Check the winner
-#   If we have a winner
-#       Print a message
-#       Break
-#   If the board is full
-#       Print a message
-#       Break
-#   Switch the current Player
-
-# Solution
 board = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
